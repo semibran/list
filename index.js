430f834b9f8efc067eab34ec6fc06971f8937488
Brandon Semilla <semibran@gmail.com>
2017-06-17 19:16:59 -0700

    simplify

--- index.js
+++ index.js
@@ -1,94 +1,36 @@
-module.exports = { append, prepend, insert, remove, splice, locate, contains, slice, clone, map, filter, find }
-
-function append(array, value) {
-	array[array.length] = value
+exports.append = function (list, item) {
+  list[list.length] = item
+  return list
 }
 
-function prepend(array, value) {
-	insert(array, value, 0)
+exports.insert = function (list, index, item) {
+  var length = list.length
+  if (index >= 0 && index < length) {
+    for (var i = length; i > index; i--) {
+      list[i] = list[i - 1]
+    }
+    list[index] = item
+  }
+  return list
 }
 
-function insert(array, value, index) {
-	if (index <= array.length) {
-		for (var i = array.length; i > index; i--) {
-			array[i] = array[i - 1]
-		}
-		array[index] = value
-	}
+exports.remove = function (list, index) {
+  var length = list.length
+  if (index >= 0 && index < length) {
+    var item = list[index]
+    for (var i = index; i < length - 1; i++) {
+      list[i] = list[i + 1]
+    }
+    list.length--
+    return item
+  }
 }
 
-function remove(array, value) {
-	var index = locate(array, value)
-	if (index !== -1) {
-		splice(array, index)
-		return true
-	} else {
-		return false
-	}
-}
-
-function splice(array, index) {
-	if (index >= 0 && index < array.length) {
-		for (var max = array.length - 1; index < max; index++) {
-			array[index] = array[index + 1]
-		}
-		array.length--
-		return true
-	} else {
-		return false
-	}
-}
-
-function locate(array, value) {
-	for (var i = 0, length = array.length; i < length; i++) {
-		if (array[i] === value) {
-			return i
-		}
-	}
-	return -1
-}
-
-function contains(array, value) {
-	return locate(array, value) !== -1
-}
-
-function slice(array, begin, end) {
-	var result = new Array(begin - end)
-	for (var i = begin; i < end; i++) {
-		result[i] = array[i]
-	}
-	return result
-}
-
-function clone(array) {
-	return slice(array, 0, array.length)
-}
-
-function map(array, callback) {
-	var length = array.length
-	var result = new Array(length)
-	for (var i = 0; i < length; i++) {
-		result[i] = callback(array[i], i)
-	}
-	return result
-}
-
-function filter(array, callback) {
-	var result = []
-	for (var i = 0, length = array.length; i < length; i++) {
-		var value = array[i]
-		if (callback(value, index)) {
-			append(result, value)
-		}
-	}
-	return result
-}
-
-function find(array, callback) {
-	for (var i = 0, length = array.length; i < length; i++) {
-		var value = array[i]
-		if (callback(value, i)) {
-			return value
-		}
-	}
+exports.locate = function (list, item) {
+  var length = list.length
+  for (var i = 0; i < length; i++) {
+    if (list[i] === item) {
+      return i
+    }
+  }
 }
---
+++ readme.md
@@ -0,0 +1,57 @@
+# list
+> Lightweight and context-free array operations
+
+## install
+```sh
+npm install semibran/list
+```
+
+## usage
+```js
+const { append, insert, remove, locate } = require('list')
+```
+
+### `append(list, item)`
+Adds `item` to the end of `list` and returns `list`.
+```js
+> append(['foo', 'bar'], 'baz')
+['foo', 'bar', 'baz']
+```
+
+### `insert(list, index, item)`
+Inserts `item` at `index` in `list`. The item in `list` located at `index` and all subsequent items are shifted by one index to accommodate `item`.
+```js
+> insert(['foo', 'baz'], 1, 'bar')
+['foo', 'bar', 'baz']
+```
+To prepend to a list, insert at index `0`.
+```js
+> insert(['bar', 'baz'], 0, 'foo')
+['foo', 'bar', 'baz']
+```
+`list` is not modified if `index` is less than `0` or greater than or equal to `list.length`. Therefore, `insert` cannot be used to add items to the end of a list - use `append` instead.
+
+### `remove(list, index)`
+Removes the item located at `index` from `list`. Returns the item if it exists, otherwise `undefined`.
+```js
+> remove(['foo', 'bar', 'baz'], 1)
+'bar'
+```
+```js
+> remove(['foo', 'bar', 'baz'], 'qux')
+undefined
+```
+
+### `locate(list, item)`
+Finds the first index where `list[index] === item`, or `undefined` if no item is found.
+```js
+> locate(['foo', 'bar', 'baz'], 'baz')
+2
+```
+```js
+> locate(['foo', 'bar', 'baz'], 'qux')
+undefined
+```
+
+## license
+[MIT](https://opensource.org/licenses/MIT) © [Brandon Semilla](https://git.io/semibran)
